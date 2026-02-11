1b91a7311b7c3bf109366837ce808b1ec4b22579
ma4etea <alexvlod85@gmail.com>
2026-02-11 18:58:27 +0300

    feat: refactor FastAPI app to modularize resources lifecycle, dependency management, and exception handling

--- src/presentation/fastapi/app.py
+++ src/presentation/fastapi/app.py
@@ -1,16 +1,45 @@
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-from src.core.settings import settings
-from src.presentation.fastapi.routers.base import public_router, protected_router
+from src.infrastructure.auth.jwt.token_service import JwtTokensService
+from src.infrastructure.db.postgres.database import Postgres
+from src.infrastructure.email.smtp import EmailService
+from src.infrastructure.factories.interactors.base import InteractorResources
+from src.infrastructure.security.codes import ConfirmCodeService
+from src.infrastructure.security.password import PasswordService
+from src.presentation.fastapi.base import routers
+from src.presentation.fastapi.base.dependencies import (
+    AppResources,
+    Settings,
+)
+from src.presentation.fastapi.base.handlers import register_exception_handlers
 
 
 @asynccontextmanager
-async def lifespan(_app: FastAPI):
+async def lifespan(app: FastAPI):
+    """Управляет жизненным циклом ресурсов приложения."""
+    settings = Settings()
+    postgres = Postgres(settings.postgres.DB_URL_ASYNC)
+    interactor_resources = InteractorResources(
+        session_factory=postgres.session_factory,
+        token_service=JwtTokensService(settings.jwt),
+        password_service=PasswordService(settings.password.DEFAULT_CONTEXT),
+        confirm_code_service=ConfirmCodeService(),
+        email_service=EmailService(),
+    )
+    app.state.resources = AppResources(
+        settings=settings,
+        interactors=interactor_resources,
+    )
+    app.root_path = settings.app.ROOT_PATH
     yield
+    await postgres.dispose()
 
 
-app = FastAPI(lifespan=lifespan, root_path=settings.ROOT_PATH)
-
-app.include_router(public_router)
-app.include_router(protected_router)
+def fastapi_app() -> FastAPI:
+    app = FastAPI(lifespan=lifespan)
+    register_exception_handlers(app)
+    app.include_router(routers.public)
+    app.include_router(routers.protected)
+    return app
