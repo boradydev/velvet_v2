0c3c13e6da4faa0425e6c88cbd9d0e55c998ebb3
ma4etea <alexvlod85@gmail.com>
2026-02-11 18:06:11 +0300

    feat: add documentation templates and registry for employee registration

---
+++ src/presentation/fastapi/employees/descriptions/registry.py
@@ -0,0 +1,15 @@
+from src.core.paths import PROJECT_DIR
+from src.presentation.fastapi.base.descriptions import registry
+
+
+template_factory = registry.DocsTemplateFactory(
+    str(PROJECT_DIR / "src/presentation/fastapi/employees")
+)
+docs_registry = registry.DocsTemplateRegistry(template_factory)
+
+docs_registry.add(
+    "descriptions/register.md",
+    str(PROJECT_DIR / "src/presentation/fastapi/employees"),
+    # expire_register=str(round(settings.REGISTRATION_EXPIRE_MINUTES / 60)),
+    # expire_code=str(settings.CONFIRM_CODE_EXPIRE_MINUTES),
+)
