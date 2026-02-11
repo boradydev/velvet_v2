b12e62b1a9f112d34c9566000576af96c2c5a5bc
ma4etea <alexvlod85@gmail.com>
2026-02-11 18:05:27 +0300

    chore: remove unused directories and refactor router structure

---
+++ src/presentation/fastapi/base/routers.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+from src.presentation.fastapi.employees.routers import employees_public_router
+
+
+public = APIRouter(
+    prefix="/public",
+)
+
+public.include_router(employees_public_router)
+
+protected = APIRouter(
+    prefix="/protected",
+    # dependencies=[Depends(verify_access_token)],
+)
