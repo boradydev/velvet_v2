d6d9d19f96125742774ece0a62c3aed90dd97a21
boradydev <alexvlod85@gmail.com>
2026-02-14 12:23:48 +0300

    chore: Refactor employee service interfaces and add ...

--- src/application/employees/interfaces.py
+++ src/application/employees/interfaces.py
@@ -1,10 +1,9 @@
 from collections.abc import Callable
 from typing import Any, Protocol
 
+from src.application.base.interfaces import IEventBus
 from src.application.employees import interactors
-from src.application.services.code import IConfirmationCodeService
 from src.application.services.logger import ILogger
-from src.application.services.send import IEmailService
 from src.application.services.token import ITokenService
 from src.domain.employees.interfaces import IPasswordService
 
@@ -14,9 +13,11 @@ class IEmployeeInteractorFactory(Protocol):
 
     def create_register_interactor(self) -> interactors.RegisterInteractor:
         """Создает сценарий регистрации нового сотрудника."""
+        ...
 
     def create_login_interactor(self) -> interactors.LoginInteractor:
         """Создает сценарий аутентификации сотрудника."""
+        ...
 
 
 class IEmployeeInteractorResources(Protocol):
@@ -25,23 +26,24 @@ class IEmployeeInteractorResources(Protocol):
     @property
     def session_factory(self) -> Callable[[], Any]:
         """Фабрика сессий. Возвращает объект сессии (AsyncSession) при вызове."""
+        ...
 
     @property
     def token_service(self) -> ITokenService:
         """Сервис для работы с JWT или OAuth2 токенами."""
+        ...
 
     @property
     def password_service(self) -> IPasswordService:
         """Сервис хеширования и проверки паролей."""
+        ...
 
     @property
-    def confirm_code_service(self) -> IConfirmationCodeService:
-        """Генератор и верификатор кодов подтверждения (OTP)."""
-
-    @property
-    def email_service(self) -> IEmailService:
-        """Транспорт для отправки уведомлений на почту."""
+    def even_bus(self) -> IEventBus:
+        """Шина событйи домена."""
+        ...
 
     @property
     def logger(self) -> ILogger:
         """Логгер."""
+        ...
