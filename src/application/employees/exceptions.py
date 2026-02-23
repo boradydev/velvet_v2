880dc965b83a71bf3fba9e7f3ddde71e0fc05aa7
boradydev <alexvlod85@gmail.com>
2026-02-23 21:22:57 +0300

    chore: Remove unused employee-related modules and classes for cleanup and optimization

--- src/application/employees/exceptions.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-from src.core.exceptions import BaseAppException
-
-
-@dataclass(frozen=True, slots=True)
-class EmployeeAlreadyExistsException(BaseAppException):
-    pass
