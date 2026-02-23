d95eb2ef0afb3c42621e93ecac7c2adbb9318ee6
boradydev <alexvlod85@gmail.com>
2026-02-23 21:47:27 +0300

    chore: Refactor employee registration endpoints and dependencies for improved clarity and consistency

--- src/presentation/fastapi/employees/controller.py
+++ src/presentation/fastapi/employees/controller.py
@@ -2,21 +2,55 @@ from typing import Annotated
 
 from fastapi import Depends
 
-from src.application.employees.dto import CredentialsEmployeeDTO
-from src.application.employees.interactors import RegisterInteractor
-from src.presentation.fastapi.base.schemas import NullDataResponse
-from src.presentation.fastapi.employees import dependencies as deps
-from src.presentation.fastapi.employees.schemas import RegisterRequest
+from src.application.employees import cases, dtos
+from src.presentation.fastapi.common.schemas import NullDataResponse
+from src.presentation.fastapi.employees import deps, schemas
+from src.presentation.fastapi.employees.interfaces.cookie import IAuthCookieManager
 
 
-async def register(
-    data: RegisterRequest,
-    interactor: Annotated[RegisterInteractor, Depends(deps.get_register_interactor)],
+async def register_employee(
+    data: schemas.RegisterRequest,
+    case: Annotated[cases.Register, Depends(deps.get_register_case)],
 ) -> NullDataResponse:
-    await interactor.register(
-        CredentialsEmployeeDTO(
+    await case.execute(
+        dtos.CredentialsEmployeeDTO(
             email=data.email,
             password=str(data.password),
         )
     )
     return NullDataResponse()
+
+
+async def resend_confirmation_code(
+    data: schemas.ResendRequest,
+    case: Annotated[cases.ReSendCodeConfirmation, Depends(deps.get_resend_case)],
+) -> NullDataResponse:
+    await case.execute(
+        dtos.ResendCodeEmployeeDTO(
+            email=data.email,
+        )
+    )
+    return NullDataResponse()
+
+
+async def confirm_employee(
+    data: schemas.ConfirmRegisterRequest,
+    case: Annotated[cases.ConfirmRegister, Depends(deps.get_confirm_register_case)],
+    cookie: Annotated[IAuthCookieManager, Depends(deps.get_auth_cookie_manager)],
+) -> schemas.AuthTokensResponse:
+    tokens = await case.execute(
+        dto=dtos.ConfirmRegisterDTO(
+            email=data.email,
+            confirmation_code=data.confirm_code,
+        )
+    )
+    cookie.set_auth_cookies(
+        access_token=tokens.access_token,
+        refresh_token=tokens.refresh_token,
+        device_id=tokens.device_id,
+    )
+    return schemas.AuthTokensResponse(
+        access_token=tokens.access_token,
+        refresh_token=tokens.refresh_token,
+        device_id=tokens.device_id,
+    )
--- src/presentation/fastapi/employees/deps.py
+++ src/presentation/fastapi/employees/deps.py
@@ -1,13 +1,38 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, Request, Response
 
-from src.application.employees import interactors
-from src.infrastructure.common.resources import LifespanState
+from src.application.employees import cases
+from src.infrastructure.common.resources import AppState, LifespanState
+from src.infrastructure.web.fastapi.cookies import AuthCookieManager
 from src.presentation.fastapi.common.common_deps import get_lifespan_state
 
 
-def get_register_interactor(
+def get_register_case(
     lifespan: Annotated[LifespanState, Depends(get_lifespan_state)],
-) -> interactors.RegisterInteractor:
-    return lifespan.interactors.employees.create_register_interactor()
+) -> cases.Register:
+    return lifespan.cases.employees.create_register_case()
+
+
+def get_confirm_register_case(
+    lifespan: Annotated[LifespanState, Depends(get_lifespan_state)],
+) -> cases.ConfirmRegister:
+    return lifespan.cases.employees.create_confirm_register_case()
+
+
+def get_resend_case(
+    lifespan: Annotated[LifespanState, Depends(get_lifespan_state)],
+) -> cases.ReSendCodeConfirmation:
+    return lifespan.cases.employees.create_resend_case()
+
+
+def get_auth_cookie_manager(
+    app_state: Annotated[AppState, Depends(get_lifespan_state)],
+    request: Request,
+    response: Response,
+) -> AuthCookieManager:
+    return AuthCookieManager(
+        request=request,
+        response=response,
+        settings=app_state.settings.jwt,
+    )
