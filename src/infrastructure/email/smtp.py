c954dd2614a1162837098ec5499d2d75ede06bb9
boradydev <alexvlod85@gmail.com>
2026-02-23 21:46:07 +0300

    chore: Refactor imports in `smtp.py` for improved consistency in interfaces and value objects

--- src/infrastructure/email/smtp.py
+++ src/infrastructure/email/smtp.py
@@ -1,5 +1,5 @@
-from src.application.services.send import IEmailService
-from src.domain.employees.value_objects import Email
+from src.domain.common.interfaces.services_abc import IEmailService
+from src.domain.employees.vals import Email
 
 
 class EmailService(IEmailService):  # todo затычка для отправки проверочного кода
