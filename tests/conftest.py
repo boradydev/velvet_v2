4f9c6cb2f70b7753756269001ba1cc2c6ff47e60
boradydev <alexvlod85@gmail.com>
2026-02-26 22:03:36 +0300

    test: Add unit tests for `ReSendCodeConfirmation` use case and new fixtures for mocking dependencies

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,12 @@ from datetime import UTC, datetime
 from unittest.mock import AsyncMock, MagicMock
 
 import pytest
+from faker import Faker
 
+from src.application.employees.services import IEmployeeTasksService
 from src.domain.common.interfaces import services_abc
 from src.domain.employees.interfaces.repos import db_abc
+from src.domain.employees.interfaces.repos.cache_abc import IConfirmationCodeRepository
 from src.domain.employees.interfaces.uow_abc import IEmployeeUOW
 from tests.unit.application.employees import fakes
 
@@ -28,6 +31,11 @@ def fake_confirmation_code_repo() -> fakes.FakeConfirmationCodeRepository:
 
 
 @pytest.fixture
+def mock_confirmation_code_repo() -> IConfirmationCodeRepository | AsyncMock:
+    return AsyncMock(spec_set=IConfirmationCodeRepository)
+
+
+@pytest.fixture
 def mock_email_service() -> services_abc.IEmailService | AsyncMock:
     return AsyncMock(spec_set=services_abc.IEmailService)
 
@@ -43,12 +51,17 @@ def mock_pass_service() -> services_abc.IPasswordService | MagicMock:
 
 
 @pytest.fixture
+def mock_task_service() -> IEmployeeTasksService | MagicMock:
+    return MagicMock(spec_set=IEmployeeTasksService)
+
+
+@pytest.fixture
 def mock_logger() -> services_abc.ILogger | MagicMock:
     return MagicMock(spec_set=services_abc.ILogger)
 
 
 @pytest.fixture
-def frozen_now():
+def frozen_now() -> datetime:
     """2026-01-01 12:00:00+00:00."""
     return datetime(2026, 1, 1, 12, 0, 0, tzinfo=UTC)
 
@@ -61,3 +74,8 @@ def mock_get_now(frozen_now):
         return frozen_now
 
     return get_frozen_now
+
+
+@pytest.fixture
+def faker() -> Faker:
+    return Faker()
