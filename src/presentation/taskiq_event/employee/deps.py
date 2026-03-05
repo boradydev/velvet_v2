90dc7303dddc72d0d71f13528e33c317e006bd9d
boradydev <alexvlod85@gmail.com>
2026-03-05 16:41:59 +0300

    refactor: Update employee task dependencies, schemas, and DTOs for improved clarity and consistency

--- src/presentation/taskiq_event/employee/deps.py
+++ src/presentation/taskiq_event/employee/deps.py
@@ -3,11 +3,11 @@ from typing import Annotated
 from taskiq_dependencies import Depends
 
 from src.application.employees.cases import SendConfirmation
-from src.infrastructure.common.resources import AppState
+from src.infrastructure.common.resources import LifespanState
 from src.presentation.fastapi.common.common_deps import get_app_state
 
 
 def get_send_code_confirmation(
-    resources: Annotated[AppState, Depends(get_app_state)],
+    resources: Annotated[LifespanState, Depends(get_app_state)],
 ) -> SendConfirmation:
     return resources.cases.employees.create_send_registration_confirmation()
